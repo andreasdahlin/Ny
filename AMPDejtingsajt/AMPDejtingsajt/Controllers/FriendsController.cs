9ebad2feee7b1a520b1c11b054345fdb63b88a24
Andreas Dahlin <andreasdahlin@live.se>
2018-01-05 19:48:12 +0100

    FriendRequests

---
+++ AMPDejtingsajt/AMPDejtingsajt/Controllers/FriendsController.cs
@@ -0,0 +1,48 @@
+﻿using AMPDejtingsajt.Models;
+using AMPDejtingsajt.Repositories;
+using AMPDejtingsajt.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMPDejtingsajt.Controllers
+{
+    public class FriendsController : Controller
+    {
+        private DataContext dataContext = new DataContext();
+        private PersonRepository personRepository;
+
+        public FriendsController()
+        {
+            personRepository = new PersonRepository(dataContext);
+        }
+
+        [HttpGet]
+        public ActionResult User(int id)
+        {
+            var model = GetFriends();
+            List<int> ids = new List<int>();
+            var friendRequests = dataContext.FriendRequest.Where(p => p.ReceiverId == id).ToList();
+
+            foreach (var item in friendRequests)
+            {
+                ids.Add(item.SenderId);
+            }
+
+            foreach (var personID in ids)
+            {
+                var userID = dataContext.User.Where(p => p.PersonID == personID);
+                model.Person = userID;
+            }
+
+            return View(model);
+        }
+
+        private FriendsViewModel GetFriends()
+        {
+            return new FriendsViewModel { Person = new List<Person>() };
+        }
+    }
+}
---
+++ AMPDejtingsajt/AMPDejtingsajt/ViewModels/FriendsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using AMPDejtingsajt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMPDejtingsajt.ViewModels
+{
+    public class FriendsViewModel
+    {
+        public IEnumerable<Person> Person { get; set; }
+    }
+}
