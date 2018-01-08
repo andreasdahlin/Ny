83ebcbdc4677136d1d095e25c9c469e29e0136e8
Andreas Dahlin <andreasdahlin@live.se>
2018-01-08 17:32:03 +0100

    Går nu att byta språk till engelska och svenska

---
+++ AMPDejtingsajt/AMPDejtingsajt/Controllers/LanguageController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMPDejtingsajt.Controllers
+{
+    public class LanguageController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Change(string LanguageAbbreviation)
+        {
+            if(LanguageAbbreviation != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(LanguageAbbreviation);
+                Thread.CurrentThread.CurrentCulture = new CultureInfo(LanguageAbbreviation);
+            }
+            HttpCookie cookie = new HttpCookie("Language");
+            cookie.Value = LanguageAbbreviation;
+            Response.Cookies.Add(cookie);
+
+            return View("Index");
+        }
+    }
+}
--- AMPDejtingsajt/AMPDejtingsajt/Internationalization/Resource.Designer.cs
+++ AMPDejtingsajt/AMPDejtingsajt/Internationalization/Resource.Designer.cs
@@ -124,6 +124,15 @@ namespace AMPDejtingsajt.Internationalization {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Language.
+        /// </summary>
+        public static string Language {
+            get {
+                return ResourceManager.GetString("Language", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to LogIn.
         /// </summary>
         public static string LogIn {
